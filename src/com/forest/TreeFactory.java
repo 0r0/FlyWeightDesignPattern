23dfb87d9dca46f06184a6c40cdf5daf9f9e93a1
0r0 <0r0.developer@gmail.com>
2020-01-17 01:50:03 +0330

    debug when treeType is not existed then add it in map

--- src/com/forest/TreeFactory.java
+++ src/com/forest/TreeFactory.java
@@ -4,11 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TreeFactory {
-    public static Map<String,TreeType> treeTypes=new HashMap<>();
+     static Map<String,TreeType> treeTypes=new HashMap<>();
     public static  TreeType getTree(String name,String kind,String color){
-        TreeType treeType=treeTypes.get(name);
+        TreeType treeType;
+        treeType = treeTypes.get(name);
         if(treeType==null) {
-            treeTypes.put(name, new TreeType(name, kind, color));
+            treeType=new TreeType(name, kind, color);
+            treeTypes.put(name, treeType);
         }
             return treeType;
         }
