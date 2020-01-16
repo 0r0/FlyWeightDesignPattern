9d6a38dfed289f3e0093c959d24fb94a1ebdde88
0r0 <0r0.developer@gmail.com>
2020-01-17 01:53:39 +0330

    sample usage of flyweight design pattern

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,21 @@
 package com.company;
 
 
+import com.forest.Forest;
+import com.forest.TreeFactory;
+import com.forest.TreeType;
 
 public class Main {
 
     public static void main(String[] args) {
+        TreeType treeType1 =TreeFactory.getTree("Bambo","Thin","Green");
+        TreeType treeType2 =TreeFactory.getTree("Peach","Fruit","Gray");
 
+        Forest f=new Forest();
+
+        f.plantTrees(treeType1.name,treeType1.kind,treeType1.color,10,20);
+        f.draw("hi");
+        f.plantTrees(treeType2.name,treeType1.kind,treeType1.color,10,20);
+        f.draw("goodbye");
     }
 }
